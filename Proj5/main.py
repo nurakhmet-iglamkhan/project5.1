55c1e69009007e4aa47a4229b127184de68ecdac
nurakhmet-iglamkhan <55078971+nurakhmet-iglamkhan@users.noreply.github.com>
2019-10-30 11:23:58 +0600

    Add files via upload

---
+++ Proj5/main.py
@@ -0,0 +1,20 @@
+def main():
+    r = open(u'file/comp2.txt', 'w')
+    l = open(u'file/comp1.txt', 'r+').read().split()
+    min=len(l[0])
+    max=len(l[0])
+    min_i=0
+    max_i=0
+    for i in range(len(l)):
+        if len(l[i])>max:
+            max=len(l[i])
+            max_i=i
+        if len(l[i])<min:
+            min=len(l[i])
+            min_i=i
+
+    r.write('max:\t' + l[max_i]+'\n')
+    r.write('min:\t' + l[min_i])
+
+if __name__ == '__main__':
+    main()
